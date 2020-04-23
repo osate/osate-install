183ec81b739b6e52dd777900112cc5f8bb183123
jjhugues <jhugues@andrew.cmu.edu>
2020-04-23 14:31:23 -0400

    * First version of OSATE install script

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# osate-install
+# OSATE Installation script
+
+This repository provides an automated installation script for OSATE
+from https://osate.org.
+
+It has been tested on Linux and macOS.
+
+
+## Usage
+
+From the terminal, simply issue the following command
+
+
+```
+./install-script.sh
+```
+
+The script will directly download the latest release of OSATE, and
+install it in the `osate2-<release>` directory.
+
+For macOS platform, the script will ask for an adminator password to
+remove `osate.app` from quarantine as detailed here:
+https://osate.org/download-and-install.html#detailed-installation-for-macos
---
+++ install-osate.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+###############################################################################
+# This script downloads and installs OSATE
+#
+###############################################################################
+
+# URL to OSATE download repository
+OSATE_DOWNLOAD_URL=https://osate-build.sei.cmu.edu/download/osate/stable/latest/products
+
+###############################################################################
+# Target specific configuration
+
+case "$(uname -s)" in
+    Darwin)
+        OSATE_FILTER="macos"
+        SED=gsed
+        ;;
+
+    Linux)
+        OSATE_FILTER="linux"
+        SED=sed
+        ;;
+esac
+
+###############################################################################
+# Download OSATE
+#################
+
+download_osate() {
+    # 1. Extract the name of the binary from OSATE index.html page
+
+    OSATE_BINARY=$(curl -s -L $OSATE_DOWNLOAD_URL | \
+                       grep -io '<a href=['"'"'"][^"'"'"']*['"'"'"]' | \
+                       $SED -e 's/^<a href=["'"'"']//i' -e 's/["'"'"']$//i' | grep $OSATE_FILTER)
+
+
+    OSATE_INSTALL_DIR=$(echo $OSATE_BINARY | cut -f1,2 -d'-')
+
+    # 2. Download iff there is no local copy, or if the size does not
+    #    match (e.g. case of an interrupted download).
+
+    local_file_size=$([[ -f ${OSATE_BINARY} ]] && wc -c < ${OSATE_BINARY} || echo "0")
+    remote_file_size=$(curl -sI $OSATE_DOWNLOAD_URL/$OSATE_BINARY | awk '/Content-Length/ { print $2 }' | tr -d '\r' )
+
+    if [[ "$local_file_size" -ne "$remote_file_size" ]]; then
+        curl $OSATE_DOWNLOAD_URL/$OSATE_BINARY $zflag --output $OSATE_BINARY
+    else
+        echo Nothing to download, exiting
+    fi
+}
+
+###############################################################################
+# Install OSATE
+###############
+
+install_osate() {
+    mkdir $OSATE_INSTALL_DIR
+
+    case "$(uname -s)" in
+        Darwin)
+            tar -C $OSATE_INSTALL_DIR -xf $OSATE_BINARY
+            echo Administrator password required to adjust quarantine status
+            ( pushd $OSATE_INSTALL_DIR ; sudo xattr -rd com.apple.quarantine osate2.app/ ; popd)
+            ;;
+
+        Linux)
+            tar -C $OSATE_INSTALL_DIR -xf $OSATE_BINARY
+            ;;
+    esac
+
+    echo Latest OSATE release installed in $OSATE_INSTALL_DIR
+}
+
+###############################################################################
+# Install plug-ins, see
+# https://www.cs.ubc.ca/~gberseth/blog/install-eclipse-plugins-via-the-command-line.html
+##################
+
+install_plugins(){
+
+    echo "Installing plug-ins"
+    case "$(uname -s)" in
+        Darwin)
+            OSATE_BIN=$OSATE_INSTALL_DIR/osate2.app/Contents/MacOS/osate
+            ;;
+
+        Linux)
+            ;;
+    esac
+
+    echo " installing Resolute"
+    $OSATE_BIN -application org.eclipse.equinox.p2.director \
+               -repository http://osate-build.sei.cmu.edu/download/osate/stable/2.3.7/updates \
+               -installIU  com.rockwellcollins.atc.resolute.feature.feature.group -nosplash
+
+    echo " installing Cheddar"
+    $OSATE_BIN -application org.eclipse.equinox.p2.director \
+               -repository https://osate-build.sei.cmu.edu/p2/ocarina \
+               -installIU  org.osate.ocarina.feature.feature.group -nosplash
+
+    echo " installing OpenModelica MDT"
+    $OSATE_BIN -application org.eclipse.equinox.p2.director \
+               -repository https://www.ida.liu.se/labs/pelab/modelica/OpenModelica/MDT/ \
+               -installIU  org.modelica.mdt.feature.group -nosplash
+
+#    echo " installing Papyrus/SysML"
+#    $OSATE_BIN -Application org.eclipse.equinox.p2.director \
+#               -repository https://download.eclipse.org/modeling/mdt/papyrus/updates/releases/2019-12/ \
+#               -installIU org.eclipse.papyrus.uml.feature.feature.group,org.eclipse.papyrus.uml.feature.feature.group,org.eclipse.papyrus.uml.alf.feature.feature.group,org.eclipse.papyrus.uml.decoratormodel.feature.feature.group,org.eclipse.papyrus.uml.architecture.feature.feature.group,org.eclipse.papyrus.uml.diagram.feature.feature.group,org.eclipse.papyrus.uml.expressions.feature.feature.group,org.eclipse.papyrus.uml.internationalization.feature.feature.group,org.eclipse.papyrus.uml.m2m.qvto.feature.feature.group,org.eclipse.papyrus.uml.modelexplorer.feature.feature.group,org.eclipse.papyrus.uml.properties.feature.feature.group,org.eclipse.papyrus.uml.search.feature.feature.group,org.eclipse.papyrus.uml.nattable.feature.feature.group,org.eclipse.papyrus.uml.textedit.feature.feature.group,org.eclipse.papyrus.uml.tools.feature.feature.group,org.eclipse.papyrus.uml.ui.feature.feature.group,org.eclipse.papyrus.uml.diagram.css.feature.feature.group,org.eclipse.papyrus.infra.gmfdiag.css.feature.feature.group,org.eclipse.papyrus.uml.xtext.integration.feature.feature.group
+
+#    $OSATE_BIN -application org.eclipse.equinox.p2.director \
+#               -repository http://download.eclipse.org/modeling/mdt/papyrus/components/sysml14/ \
+#               -installIU  org.eclipse.papyrus.sysml14.feature.feature.group -nosplash
+}
+
+###############################################################################
+# Main processing starts here
+
+download_osate
+install_osate
+#install_plugins
